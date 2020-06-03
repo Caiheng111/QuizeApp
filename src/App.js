2c17b89d4daafc446d57628a8aef9d82db3ed9ac
Cai heng <ianyaheng822@gmail.com>
2020-06-03 18:17:45 +1000

    render the questions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
 
+import React, { Component } from 'react'
 import './App.css';
+import quizService from './quizService/index'
 
-function App() {
-  return (
-    <div className="container">
-      <div className="title">Quizbee App</div>
-    </div>
-  );
+class App extends Component {
+  state={
+    questionsBank:[]
+  }
+
+
+  getQuestions =()=>{
+    quizService().then((questions)=>{
+      this.setState({
+        questionsBank: questions
+      })
+    })
+  }
+  componentDidMount(){
+    this.getQuestions()
+  }
+
+  
+  render() {
+    return (
+      <div className="container">
+         <div className="title">Quizbee App</div>
+         {this.state.questionsBank.length>0 && this.state.questionsBank.map((question,answers,correct,questionId)=>
+           <h4>{question}</h4>
+         )}
+      </div>
+    )
+  }
 }
 
 export default App;
+
+
